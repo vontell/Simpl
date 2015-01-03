487bf5b8cd0fd7d564a8bf94e88582a416ff4d48
Aaron Vontell <vontell@mit.edu>
2015-01-03 15:39:49 -0500

    Initial Commit

---
+++ wear/src/main/java/com/simpl/simpl/TaskListActivity.java
@@ -0,0 +1,24 @@
+package com.simpl.simpl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class TaskListActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
